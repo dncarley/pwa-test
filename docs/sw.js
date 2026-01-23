fbfc13d37ba788bd9bcb0ab4d636291d897beba5
Dave Carley <dncarley@gmail.com>
2026-01-22 20:45:15 -0500

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,11 +1,22 @@
+const CACHE = "races-v0.1";
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("pico").then(c =>
-      c.addAll([
-        "index.html",
-        "app.js",
-        "manifest.json"
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./app.js",
+        "./vision.js",
+        "./ui.js",
+        "./style.css"
       ])
     )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
